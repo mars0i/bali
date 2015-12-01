bc59ef8e95bd1134608d6a3da9d0079d130489d3
mars0i <mabrams@uab.edu>
2015-12-01 00:46:55 -0600

    misc tweaks to get it to compile, and load in NetLogo

--- src/extensions/popcobali/src/PopcoBali.java
+++ src/extensions/popcobali/src/PopcoBali.java
@@ -4,7 +4,7 @@ import java.net.*;
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 //import clojure.pprint.*;
-import popco.core.main.*;
+import popco.core.*;
 
 public class PopcoBali extends DefaultReporter {
 	public Syntax getSyntax() {
@@ -13,10 +13,9 @@ public class PopcoBali extends DefaultReporter {
 
 	public Object report(Argument args[], Context context) throws ExtensionException {
 		try {
-			addPath("extensions/clojure/clojure-1.7.0.jar"); // (addPath() defined below) <== PUT MY POPCO JAR FILE HERE
-			// Will throw exceptions without the preceding line
-			IFn cljFn = Clojure.var("popco.core.main", "netlogo-test"); // IS THIS REALLY NECESSARY??
-			Object retObj = cljfn.invoke(cljFn.invoke(args[0])); // Now Clojure it.
+			addPath("extensions/popcobali/popcobali.jar"); // (addPath() defined below) <== PUT MY POPCO JAR FILE HERE
+			IFn cljFn = Clojure.var("popco.core.main", "netlogo-test"); // IS THIS REALLY NECESSARY??  Can't I just call Clojure directly?
+			Object retObj = cljFn.invoke(cljFn.invoke(args[0])); // Now Clojure it.
 			if (retObj == null) { return "nil"; }{ return retObj.toString(); }
 		} catch (Throwable e) {
 			throw new ExtensionException( e.getMessage() ) ;
